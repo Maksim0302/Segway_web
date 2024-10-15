5beadb6e124ee2b3f06b5a5103f5a9f46fc1627b
Maksim <106908147+Maksim0302@users.noreply.github.com>
2024-10-15 19:46:31 +0300

    add build

--- src/components/Owner/Owner.jsx
+++ src/components/Owner/Owner.jsx
@@ -1,35 +1,33 @@
-import React from 'react';
-import './Owner.scss';
-import logo from '../../images/logo.png';
-import avatar from '../../images/avatar.png';
+import React from 'react'
+import './Owner.scss'
+import logo from '../../images/logo.png'
+import avatar from '../../images/avatar.png'
 
 const Owner = () => {
   return (
     <section className="owner">
-        <div className="container">
-            <div className="owner__content">
-                <div className="owner__logo">
-                    <a href='#logo'>
-                        <img src={logo} alt="logo" />
-                    </a>
-                </div>
-                <div className="owner__avatar">
-                    <div className="owner__avatar__image">
-                        <img src={avatar} alt="" />
-                    </div>
-                    <div className="owner__avatar__text">
-                        <div className="owner__avatar__text__name">
-                            JOHN SMITH
-                        </div>
-                        <div className="owner__avatar__text__description">
-                            Ask a question <br /> to the director
-                        </div>
-                    </div>
-                </div>
+      <div className="container">
+        <div className="owner__content">
+          <div className="owner__logo">
+            <button>
+              <img src={logo} alt="logo" />
+            </button>
+          </div>
+          <div className="owner__avatar">
+            <div className="owner__avatar__image">
+              <img src={avatar} alt="" />
             </div>
+            <div className="owner__avatar__text">
+              <div className="owner__avatar__text__name">JOHN SMITH</div>
+              <div className="owner__avatar__text__description">
+                Ask a question <br /> to the director
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </section>
   )
 }
 
-export default Owner;
+export default Owner
